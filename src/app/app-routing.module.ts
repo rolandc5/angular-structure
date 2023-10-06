dc317c5b084ac16a4211a3b0a50e847992f3874d
Roland Canuto <roland@Rolands-MacBook-Pro.local>
2023-10-05 17:11:01 -0700

    initial commit for file structure reference

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'sample',
+    loadChildren: () =>
+      import('./features/sample-feature/sample-feature-routing.module').then((m) => m.SampleFeatureRoutingModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
